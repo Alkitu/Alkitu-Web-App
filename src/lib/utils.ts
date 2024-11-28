fa335cd3e5590f499a72ae339ab0fc244b8fa90a
¨LuisEUM¨ <luiseum95@gmail.com>
2024-11-29 00:18:58 +0100

    radix dependencies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,6 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
-import { Inter, Merriweather } from "next/font/google";
-
-export const inter = Inter({ subsets: ["latin"] });
-
-export const merriweather = Merriweather({
-  subsets: ["latin"],
-  weight: ["700", "900"],
-  style: ["italic", "normal"],
-});
-
-
-export function generatePassword(length = 12) {
-  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
-  let password = ""
-  for (let i = 0; i < length; i++) {
-    password += charset.charAt(Math.floor(Math.random() * charset.length))
-  }
-  return password
+  return twMerge(clsx(inputs))
 }
