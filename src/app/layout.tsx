42d7e77b54f82ad322dfb6a0461e59a074cc0e5f
¨LuisEUM¨ <luiseum95@gmail.com>
2024-11-29 00:23:38 +0100

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 // src/app/layout.tsx
-import { ThemeProvider } from "@/context/ThemeProvider";
-import { TranslationsProvider } from "@/context/TranslationContext";
-import defaultTranslations from "@/locales/es/common.json"; // Asegúrate de que esta importación sea correcta
 import "@/app/styles/globals.css";
-import TailwindGrid from "@/components/shared/TailwindGrid";
 import { cn, inter } from "@/lib/utils";
-import { LanguageSwitchButton } from "@/components/shared/LanguageSwitchButton";
-import ThemeToggle from "@/components/shared/ThemeToggle";
-import { Toaster } from "sonner";
 
 export default async function RootLayout({
   children,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Inter, Merriweather } from "next/font/google";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const merriweather = Merriweather({
+  subsets: ["latin"],
+  weight: ["700", "900"],
+  style: ["italic", "normal"],
+});
